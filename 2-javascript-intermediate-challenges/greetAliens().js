2f7a5bad618e443a33f8daf42789c81dca37f7cc
Laura Vital Cacique <laura_vitalc@hotmail.com>
2022-01-08 12:59:28 -0300

    Intermediate challenges added!

---
+++ 2-javascript-intermediate-challenges/greetAliens().js
@@ -0,0 +1,5 @@
+const greetAliens = arr => {
+    for (let i = 0; i < arr.length; i++) {
+          console.log('Oh powerful ' + arr[i] + ', we humans offer our unconditional surrender!');
+    }
+}
