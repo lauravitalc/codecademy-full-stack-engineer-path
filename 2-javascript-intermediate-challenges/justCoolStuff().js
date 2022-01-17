5686241135a574b45fe5c64e705620c8eeb79cfa
Laura Vital Cacique <laura_vitalc@hotmail.com>
2022-01-17 19:42:40 -0300

    Novos exercícios!

---
+++ 2-javascript-intermediate-challenges/justCoolStuff().js
@@ -0,0 +1,7 @@
+const justCoolStuff = (firstArray, secondArray) => firstArray.filter(item => secondArray.includes(item))
+
+const coolStuff = ['gameboys', 'skateboards', 'backwards hats', 'fruit-by-the-foot', 'pogs', 'my room', 'temporary tattoos'];
+
+const myStuff = [ 'rules', 'fruit-by-the-foot', 'wedgies', 'sweaters', 'skateboards', 'family-night', 'my room', 'braces', 'the information superhighway']; 
+
+console.log(justCoolStuff(myStuff, coolStuff))
